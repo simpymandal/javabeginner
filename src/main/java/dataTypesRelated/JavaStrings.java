93b3bed5d525a84d450291023846e2b83babdfe5
simpymandal <datalake.solutions@gmail.com>
2023-12-20 22:12:24 +0530

    packages added

---
+++ src/main/java/dataTypesRelated/JavaStrings.java
@@ -0,0 +1,17 @@
+package dataTypesRelated;
+
+public class JavaStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JavaStrings javaStrings = new JavaStrings();
+		javaStrings.codeDeclarations();
+
+	}
+	
+	
+	public void codeDeclarations() {
+		System.out.println("codeDeclarations");
+	}
+
+}
